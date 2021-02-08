5503abe119bed767cdf8d86051facf8caaa8c0a6
Mauko Maunde <maukoese@gmail.com>
2021-02-08 10:40:20 +0300

    Revert composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6300fdcbf447acf4dd600da452fb65ec91480dfa',
+    'name' => 'osenco/wc-mpesa',
+  ),
+  'versions' => 
+  array (
+    'osenco/wc-mpesa' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6300fdcbf447acf4dd600da452fb65ec91480dfa',
+    ),
+  ),
+);
