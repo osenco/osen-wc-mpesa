79fb7a563ef5ad6a3dad87d37f48d2b5f11d69b9
Mauko Maunde <maukoese@gmail.com>
2021-12-02 22:16:09 +0300

    Add filter orders by payment gateway used

--- src/Post/Metaboxes/C2B.php
+++ src/Post/Metaboxes/C2B.php
@@ -16,6 +16,8 @@ class C2B
     {
         add_action('add_meta_boxes', [$this, 'mpesa_mb_sm']);
         add_action('save_post', array($this, 'save_order'), 10, 3);
+        add_action('restrict_manage_posts', array($this, 'filter_orders_by_payment_method'), 20);
+        add_filter('request',               array($this, 'filter_orders_by_payment_method_query'));
     }
 
     public function mpesa_mb_sm()
@@ -115,5 +117,57 @@ class C2B
                 }
             }
         }
+    }
+
+    /**
+     * Add bulk filter for orders by payment method
+     *
+     * @since 1.0.0
+     */
+    public function filter_orders_by_payment_method()
+    {
+        global $typenow;
+
+        if ('shop_order' === $typenow) {
+
+            // get all payment methods, even inactive ones
+            $gateways = WC()->payment_gateways->payment_gateways();
+
+?>
+            <select name="_shop_order_payment_method" id="dropdown_shop_order_payment_method">
+                <option value="">
+                    <?php esc_html_e('All Payment Methods', 'wc-filter-orders-by-payment'); ?>
+                </option>
+
+                <?php foreach ($gateways as $id => $gateway) : ?>
+                    <option value="<?php echo esc_attr($id); ?>" <?php echo esc_attr(isset($_GET['_shop_order_payment_method']) ? selected($id, $_GET['_shop_order_payment_method'], false) : ''); ?>>
+                        <?php echo esc_html($gateway->get_method_title()); ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+<?php
+        }
+    }
+
+
+    /**
+     * Process bulk filter order payment method
+     *
+     * @since 1.0.0
+     *
+     * @param array $vars query vars without filtering
+     * @return array $vars query vars with (maybe) filtering
+     */
+    public function filter_orders_by_payment_method_query($vars)
+    {
+        global $typenow;
+
+        if ('shop_order' === $typenow && isset($_GET['_shop_order_payment_method']) && !empty($_GET['_shop_order_payment_method'])) {
+
+            $vars['meta_key']   = '_payment_method';
+            $vars['meta_value'] = wc_clean($_GET['_shop_order_payment_method']);
+        }
+
+        return $vars;
     }
 }
